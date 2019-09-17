ad224bd47c6dfb98fa83f1468e9c1ef64821854f
Logan Saether <x@logansaether.com>
2019-09-17 20:19:40 +0200

    Update pictures

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -29,46 +29,10 @@ class HomeSplash extends React.Component {
       </div>
     );
 
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
-    const ProjectTitle = () => (
-      <h2 className="projectTitle">
-        {siteConfig.title}
-        <small>{siteConfig.tagline}</small>
-      </h2>
-    );
-
-    const PromoSection = props => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
-    );
-
     return (
       <SplashContainer>
         <div className="inner">
           <img src='/polkadot-wiki-i18n/img/Polkadot_logotype_color.svg' height={320}/>
-          {/* <ProjectTitle siteConfig={siteConfig} /> */}
-          {/* <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Build</Button>
-            <Button href={docUrl('doc2.html')}>Learn</Button>
-            <Button href={docUrl('doc2.html')}>Maintain</Button>
-          </PromoSection> */}
         </div>
       </SplashContainer>
     );
@@ -91,31 +55,6 @@ class Index extends React.Component {
           layout={props.layout}
         />
       </Container>
-    );
-
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
-      </div>
-    );
-
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
-            image: `${baseUrl}img/undraw_code_review.svg`,
-            imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations',
-          },
-        ]}
-      </Block>
     );
 
     const Description = () => (
@@ -151,55 +90,18 @@ class Index extends React.Component {
         {[
           {
             content: 'Run a validator and help to secure the Polkadot network while earning rewards.',
-            image: `${baseUrl}img/undraw_react.svg`,
+            image: `${baseUrl}img/icon_validator.svg`,
             imageAlign: 'top',
             title: 'Validate',
           },
           {
             content: 'Run a collator to package the parachain blocks and pass them to validators for verification.',
-            image: `${baseUrl}img/undraw_operating_system.svg`,
+            image: `${baseUrl}img/icon_collator.svg`,
             imageAlign: 'top',
             title: 'Collate',
           },
         ]}
       </Block>
-    );
-
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
-
-      const showcase = siteConfig.users
-        .filter(user => user.pinned)
-        .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
-
-      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
-
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
-          </div>
-        </div>
-      );
-    };
-
-    const Button = props => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
     );
 
     return (
