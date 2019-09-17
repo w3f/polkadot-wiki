9813996002f92170ccbcb608183c28c90bd94a53
Logan Saether <x@logansaether.com>
2019-09-17 13:02:24 +0200

    Some touchups

--- website/languages.js
+++ website/languages.js
@@ -12,7 +12,7 @@ const languages = [
     tag: 'en',
   },
   {
-    enabled: true,
+    enabled: false,
     name: '日本語',
     tag: 'ja',
   },
@@ -52,7 +52,7 @@ const languages = [
     tag: 'el',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Español',
     tag: 'es-ES',
   },
@@ -97,7 +97,7 @@ const languages = [
     tag: 'af',
   },
   {
-    enabled: true,
+    enabled: false,
     name: '한국어',
     tag: 'ko',
   },
@@ -137,7 +137,7 @@ const languages = [
     tag: 'ro',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Русский',
     tag: 'ru',
   },
@@ -157,7 +157,7 @@ const languages = [
     tag: 'sv-SE',
   },
   {
-    enabled: true,
+    enabled: false,
     name: 'Türkçe',
     tag: 'tr',
   },
@@ -176,6 +176,11 @@ const languages = [
     name: '中文',
     tag: 'zh-CN',
   },
-  {enabled: false, name: '繁體中文', tag: 'zh-TW'},
+  {
+    enabled: false,
+    name: '繁體中文',
+    tag: 'zh-TW',
+  },
 ];
+
 module.exports = languages;
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,93 +10,94 @@
 
 // List of projects/orgs using your project for the users page.
 const users = [
-    {
-	caption: 'User1',
-	// You will need to prepend the image path with your baseUrl
-	// if it is not '/', like: '/test-site/img/image.jpg'.
-	image: '/img/undraw_open_source.svg',
-	infoLink: 'https://www.facebook.com',
-	pinned: true,
-    },
+  {
+    caption: 'User1',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/undraw_open_source.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
 ];
 
 const siteConfig = {
-    title: 'Polkadot Wiki', // Title for your website.
-    tagline: 'The hub for those interested in learning, building, or running a node on the Polkadot protocol',
-    url: 'https://w3f.github.io', // Your website URL
-    baseUrl: '/polkadot-wiki-i18n/',
-    projectName: 'polkadot-wiki-i18n',
-    organizationName: 'w3f',
+  title: 'Polkadot Wiki', // Title for your website.
+  tagline: 'The hub for those interested in learning, building, or running a node on the Polkadot protocol',
+  url: 'https://w3f.github.io', // Your website URL
+  baseUrl: '/polkadot-wiki-i18n/',
+  projectName: 'polkadot-wiki-i18n',
+  organizationName: 'w3f',
 
-    // For no header links in the top nav bar -> headerLinks: [],
-    headerLinks: [
-	{doc: 'polkadot-build-index', label: 'Build'},
-	{doc: 'polkadot-learn-index', label: 'Learn'},
-        {doc: 'polkadot-node-index', label: 'Maintainers'},
-	{ languages: true }
+  // For no header links in the top nav bar -> headerLinks: [],
+  headerLinks: [
+    {doc: 'polkadot-build-index', label: 'Build'},
+    {doc: 'polkadot-learn-index', label: 'Learn'},
+    {doc: 'polkadot-node-index', label: 'Maintainers'},
+    { languages: true }
+  ],
+
+  // If you have users set above, you add it here:
+  users,
+
+  /* path to images for header/footer */
+  headerIcon: 'img/favicon.ico',
+  footerIcon: 'img/favicon.ico',
+  favicon: 'img/favicon.ico',
+
+  /* Colors for website */
+  colors: {
+    primaryColor: '#000000',
+    secondaryColor: '#3c0407',
+  },
+
+  /* Custom fonts for website */
+  /*
+    fonts: {
+    myFont: [
+    "Times New Roman",
+    "Serif"
     ],
-
-    // If you have users set above, you add it here:
-    users,
-
-    /* path to images for header/footer */
-    headerIcon: 'img/favicon.ico',
-    footerIcon: 'img/favicon.ico',
-    favicon: 'img/favicon.ico',
-
-    /* Colors for website */
-    colors: {
-	primaryColor: '#000000',
-	secondaryColor: '#3c0407',
+    myOtherFont: [
+    "-apple-system",
+    "system-ui"
+    ]
     },
+  */
 
-    /* Custom fonts for website */
-    /*
-      fonts: {
-      myFont: [
-      "Times New Roman",
-      "Serif"
-      ],
-      myOtherFont: [
-      "-apple-system",
-      "system-ui"
-      ]
-      },
-    */
+  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+  copyright: `Copyright © ${new Date().getFullYear()} Web3 Foundation`,
 
-    // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-    copyright: `Copyright © ${new Date().getFullYear()} Web3 Foundation`,
+  highlight: {
+    // Highlight.js theme to use for syntax highlighting in code blocks.
+    theme: 'default',
+  },
 
-    highlight: {
-	// Highlight.js theme to use for syntax highlighting in code blocks.
-	theme: 'default',
-    },
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: ['https://buttons.github.io/buttons.js'],
 
-    // Add custom scripts here that would be placed in <script> tags.
-    scripts: ['https://buttons.github.io/buttons.js'],
+  // On page navigation for the current documentation page.
+  onPageNav: 'separate',
+  
+  // No .html extensions for paths.
+  cleanUrl: true,
 
-    // On page navigation for the current documentation page.
-    onPageNav: 'separate',
-    // No .html extensions for paths.
-    cleanUrl: true,
+  // Open Graph and Twitter card images.
+  ogImage: 'img/undraw_online.svg',
+  twitterImage: 'img/undraw_tweetstorm.svg',
 
-    // Open Graph and Twitter card images.
-    ogImage: 'img/undraw_online.svg',
-    twitterImage: 'img/undraw_tweetstorm.svg',
+  // For sites with a sizable amount of content, set collapsible to true.
+  // Expand/collapse the links and subcategories under categories.
+  docsSideNavCollapsible: true,
 
-    // For sites with a sizable amount of content, set collapsible to true.
-    // Expand/collapse the links and subcategories under categories.
-    docsSideNavCollapsible: true,
+  // Show documentation's last contributor's name.
+  enableUpdateBy: true,
 
-    // Show documentation's last contributor's name.
-    enableUpdateBy: true,
+  // Show documentation's last update time.
+  enableUpdateTime: true,
 
-    // Show documentation's last update time.
-    enableUpdateTime: true,
-
-    // You may provide arbitrary config keys to be used as needed by your
-    // template. For example, if you need your repo's URL...
-      repoUrl: 'https://github.com/paritytech/polkadot',
+  // You may provide arbitrary config keys to be used as needed by your
+  // template. For example, if you need your repo's URL...
+  repoUrl: 'https://github.com/paritytech/polkadot',
 };
 
 module.exports = siteConfig;
