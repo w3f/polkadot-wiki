461c67d5124fa38a7fe646d4f4a07fe738b889b5
Logan Saether <x@logansaether.com>
2019-09-27 18:20:18 +0200

    Add a couple things to the site configuration

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,25 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Polkadot Wiki', // Title for your website.
   tagline: 'The hub for those interested in learning, building, or running a node on the Polkadot protocol.',
@@ -36,9 +14,6 @@ const siteConfig = {
     {doc: 'maintain-index', label: 'Maintain'},
     { languages: true }
   ],
-
-  // If you have users set above, you add it here:
-  users,
 
   /* path to images for header/footer */
   headerIcon: 'img/favicon.ico',
@@ -85,10 +60,6 @@ const siteConfig = {
   // No .html extensions for paths.
   cleanUrl: true,
 
-  // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
-
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
   docsSideNavCollapsible: true,
@@ -99,8 +70,15 @@ const siteConfig = {
   // Show documentation's last update time.
   enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
+  editUrl: 'https://github.com/w3f/polkadot-wiki/edit/master/docs/',
+
+  translationRecruitingLink: 'https://crowdin.com/project/polkadot-wiki',
+
+  scrollToTop: true,
+  scrollToTopOptions: {
+    zIndex: 100,
+  },
+
   repoUrl: 'https://github.com/paritytech/polkadot',
 };
 
