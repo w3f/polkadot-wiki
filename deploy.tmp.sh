9a7d5a6ff4c2698f0b0a7cb8274ac3df77b882b9
x@logansaether.com <x@logansaether.com>
2020-06-18 16:41:00 +0200

    circle changes

---
+++ deploy.tmp.sh
@@ -0,0 +1,9 @@
+# GIT_USER=lsaether PUBLISHING=true \
+#   CURRENT_BRANCH=master \
+#   USE_SSH=true \
+#   yarn kusama:publish-gh-pages
+
+GIT_USER=lsaether PUBLISHING=true \
+  CURRENT_BRANCH=master \
+  USE_SSH=true \
+  yarn polkadot:publish-gh-pages
