f082216e74389f29911b7cadaa991a0b77679cff
x@logansaether.com <x@logansaether.com>
2020-06-19 11:14:00 +0200

    Fix alias

--- deploy.tmp.sh
+++
@@ -1,9 +0,0 @@
-# GIT_USER=lsaether PUBLISHING=true \
-#   CURRENT_BRANCH=master \
-#   USE_SSH=true \
-#   yarn kusama:publish-gh-pages
-
-GIT_USER=lsaether PUBLISHING=true \
-  CURRENT_BRANCH=master \
-  USE_SSH=true \
-  yarn polkadot:publish-gh-pages
