6275d06403e856a38a9f24828fdbd814d5ec935e
Logan Saether <x@logansaether.com>
2020-06-26 12:19:52 +0200

    Kusama deployment (#612)
    * Add circleci job to deploy kusama guide
    
    * Activate test deployment
    
    * Only master

--- kusama-guide/siteConfig.js
+++ kusama-guide/siteConfig.js
@@ -6,10 +6,10 @@ const isPub = process.env.PUBLISHING;
 const siteConfig = {
   title: 'Kusama Guide', // Title for your website.
   tagline: 'One-stop-shop for Kusama information.',
-  url: isPub ? "https://lsaether.github.io/" : "", // Your website URL
-  baseUrl: isBuilding ? baseUrlPattern : (isPub ? "/test/" : "/"),
-  projectName: isPub ? "test" : "",
-  organizationName: 'lsaether',
+  url: isPub ? "https://w3f.github.io/" : "", // Your website URL
+  baseUrl: isBuilding ? baseUrlPattern : (isPub ? "/kusama-guide-hosting/" : "/"),
+  projectName: isPub ? "kusama-guide-hosting" : "",
+  organizationName: 'w3f',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
