fb8fa17575756e4ba606de3151d958fcd846e222
Bruno Skvorc <bruno@skvorc.me>
2020-07-03 11:22:00 +0200

    Remove Google Analytics

--- kusama-guide/siteConfig.js
+++ kusama-guide/siteConfig.js
@@ -1,50 +1,47 @@
-const { baseUrlPattern } = require('../scripts/utils');
+const { baseUrlPattern } = require("../scripts/utils");
 
 const isBuilding = process.env.BUILDING;
 const isPub = process.env.PUBLISHING;
 
 const siteConfig = {
-  title: 'Guide', // Title for your website.
-  tagline: 'One-stop-shop for Kusama information.',
+  title: "Guide", // Title for your website.
+  tagline: "One-stop-shop for Kusama information.",
   url: isPub ? "https://guide.kusama.network" : "", // Your website URL
   baseUrl: isBuilding ? baseUrlPattern : "/",
   projectName: isPub ? "kusama-guide-hosting" : "",
-  organizationName: 'w3f',
+  organizationName: "w3f",
   cname: "guide.kusama.network",
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'kusama-claims', label: 'Get Started' },
-    { doc: 'kusama-index', label: 'Docs' },
+    { doc: "kusama-claims", label: "Get Started" },
+    { doc: "kusama-index", label: "Docs" },
     { search: true },
-    { languages: true }
+    { languages: true },
   ],
 
   /* path to images for header/footer */
-  headerIcon: './img/Kusama_logotype_white_large.png',
-  footerIcon: './img/kusama-brand-assets/Kusama_Canary_white.png',
-  favicon: './img/kusama-brand-assets/Kusama_Canary_white.png',
+  headerIcon: "./img/Kusama_logotype_white_large.png",
+  footerIcon: "./img/kusama-brand-assets/Kusama_Canary_white.png",
+  favicon: "./img/kusama-brand-assets/Kusama_Canary_white.png",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#E6007A',
-    secondaryColor: '#000000',
+    primaryColor: "#E6007A",
+    secondaryColor: "#000000",
   },
 
   algolia: {
-    apiKey: '8bfa06b56bb8f33e5698c7f40b00b38f',
-    indexName: 'polkadot',
+    apiKey: "8bfa06b56bb8f33e5698c7f40b00b38f",
+    indexName: "polkadot",
     algoliaOptions: {
-      facetFilters: ["language:LANGUAGE"]
-    } // Optional, if provided by Algolia
+      facetFilters: ["language:LANGUAGE"],
+    }, // Optional, if provided by Algolia
   },
 
   /* Custom fonts for website */
   fonts: {
-    myFont: [
-    'Muli',
-    'sans-serif',
-    ],
+    myFont: ["Muli", "sans-serif"],
   },
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
@@ -52,28 +49,27 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default",
   },
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    'https://buttons.github.io/buttons.js',
-    'https://wiki.polkadot.network/js/load.js',
-    'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.js',
-    './js/custom.js',
-    './js/klaro-config.js',
-    './js/klaro.js'
+    "https://buttons.github.io/buttons.js",
+    "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.js",
+    "./js/custom.js",
+    "./js/klaro-config.js",
+    "./js/klaro.js",
   ],
 
   stylesheets: [
-    'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css',
-    'https://fonts.googleapis.com/css?family=Muli&display=swap',
-    './css/klaro.css'
+    "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css",
+    "https://fonts.googleapis.com/css?family=Muli&display=swap",
+    "./css/klaro.css",
   ],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  
+  onPageNav: "separate",
+
   // No .html extensions for paths.
   cleanUrl: !isBuilding,
 
@@ -89,20 +85,20 @@ const siteConfig = {
 
   markdownPlugins: [
     (md) => {
-      md.use(require('remarkable-katex'));
+      md.use(require("remarkable-katex"));
     },
   ],
 
-  editUrl: 'https://github.com/w3f/polkadot-wiki/edit/master/docs/',
+  editUrl: "https://github.com/w3f/polkadot-wiki/edit/master/docs/",
 
-  translationRecruitingLink: 'https://crowdin.com/project/polkadot-wiki',
+  translationRecruitingLink: "https://crowdin.com/project/polkadot-wiki",
 
   scrollToTop: true,
   scrollToTopOptions: {
     zIndex: 100,
   },
 
-  repoUrl: 'https://github.com/paritytech/polkadot',
+  repoUrl: "https://github.com/paritytech/polkadot",
 };
 
 module.exports = siteConfig;
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,52 +1,49 @@
-const { baseUrlPattern } = require('../scripts/utils');
+const { baseUrlPattern } = require("../scripts/utils");
 
 const isBuilding = process.env.BUILDING;
 const isPub = process.env.PUBLISHING;
 
 const siteConfig = {
-  title: 'Polkadot Wiki', // Title for your website.
-  tagline: 'The hub for those interested in learning, building, or running a node on Polkadot.',
+  title: "Polkadot Wiki", // Title for your website.
+  tagline: "The hub for those interested in learning, building, or running a node on Polkadot.",
   // url: 'https://wiki.polkadot.network', // Your website URL
   baseUrl: isBuilding ? baseUrlPattern : "/",
-  projectName: 'polkadot-wiki',
-  organizationName: 'w3f',
-  cname: 'wiki.polkadot.network',
+  projectName: "polkadot-wiki",
+  organizationName: "w3f",
+  cname: "wiki.polkadot.network",
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: 'learn-introduction', label: 'Learn' },
-    { doc: 'build-index', label: 'Build' },
-    { doc: 'maintain-index', label: 'Maintain '},
-    { href: 'https://guide.kusama.network', label: 'Kusama' },
+    { doc: "learn-introduction", label: "Learn" },
+    { doc: "build-index", label: "Build" },
+    { doc: "maintain-index", label: "Maintain " },
+    { href: "https://guide.kusama.network", label: "Kusama" },
     { search: true },
-    { languages: true }
+    { languages: true },
   ],
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#E6007A',
-    secondaryColor: '#000000',
+    primaryColor: "#E6007A",
+    secondaryColor: "#000000",
   },
 
   algolia: {
-    apiKey: '8bfa06b56bb8f33e5698c7f40b00b38f',
-    indexName: 'polkadot',
+    apiKey: "8bfa06b56bb8f33e5698c7f40b00b38f",
+    indexName: "polkadot",
     algoliaOptions: {
-      facetFilters: ["language:LANGUAGE"]
-    } // Optional, if provided by Algolia
+      facetFilters: ["language:LANGUAGE"],
+    }, // Optional, if provided by Algolia
   },
 
   /* Custom fonts for website */
   fonts: {
-    myFont: [
-    'Muli',
-    'sans-serif',
-    ],
+    myFont: ["Muli", "sans-serif"],
   },
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
@@ -54,28 +51,27 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default",
   },
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    'https://buttons.github.io/buttons.js',
-    'https://wiki.polkadot.network/js/load.js',
-    'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.js',
-    '/js/custom.js',
-    '/js/klaro-config.js',
-    '/js/klaro.js'
+    "https://buttons.github.io/buttons.js",
+    "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.js",
+    "/js/custom.js",
+    "/js/klaro-config.js",
+    "/js/klaro.js",
   ],
 
   stylesheets: [
-    'https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css',
-    'https://fonts.googleapis.com/css?family=Muli&display=swap',
-    '/css/klaro.css'
+    "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css",
+    "https://fonts.googleapis.com/css?family=Muli&display=swap",
+    "/css/klaro.css",
   ],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  
+  onPageNav: "separate",
+
   // No .html extensions for paths.
   cleanUrl: !isBuilding,
 
@@ -91,20 +87,20 @@ const siteConfig = {
 
   markdownPlugins: [
     (md) => {
-      md.use(require('remarkable-katex'));
+      md.use(require("remarkable-katex"));
     },
   ],
 
-  editUrl: 'https://github.com/w3f/polkadot-wiki/edit/master/docs/',
+  editUrl: "https://github.com/w3f/polkadot-wiki/edit/master/docs/",
 
-  translationRecruitingLink: 'https://crowdin.com/project/polkadot-wiki',
+  translationRecruitingLink: "https://crowdin.com/project/polkadot-wiki",
 
   scrollToTop: true,
   scrollToTopOptions: {
     zIndex: 100,
   },
 
-  repoUrl: 'https://github.com/paritytech/polkadot',
+  repoUrl: "https://github.com/paritytech/polkadot",
 };
 
 module.exports = siteConfig;
