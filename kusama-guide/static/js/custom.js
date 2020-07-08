c1938566975ec096d798f64d13e6a07c08e491b3
Bruno Skvorc <bruno@skvorc.me>
2020-07-08 21:42:02 +0200

    Removed github banner from kusama homepage

--- kusama-guide/static/js/custom.js
+++ kusama-guide/static/js/custom.js
@@ -80,13 +80,21 @@ docReady(function () {
   // Custom logic goes here
 
   // Insert "Improve this wiki ribbon"
-  let link = document.createElement("a");
-  link.classList = "github-fork-ribbon";
-  link.setAttribute("href", "contributing");
-  link.setAttribute("title", "Contribute to this wiki");
-  link.dataset.ribbon = "Contribute to this wiki";
-  link.innerText = "Contribute to this wiki";
-  document.querySelector("body").appendChild(link);
+  if (
+    window.location.pathname !== "/" &&
+    window.location.pathname !== "/index.html" &&
+    window.location.pathname !== "/en"
+  ) {
+    let link = document.createElement("a");
+    link.classList = "github-fork-ribbon";
+    link.setAttribute("href", "contributing");
+    link.setAttribute("title", "Contribute to this wiki");
+    link.dataset.ribbon = "Contribute to this wiki";
+    link.innerText = "Contribute to this wiki";
+    document.querySelector("body").appendChild(link);
+  } else {
+    document.querySelector(".fixedHeaderContainer").classList += " nomargin";
+  }
   // -----------------
 
   // Add fathom to footer
