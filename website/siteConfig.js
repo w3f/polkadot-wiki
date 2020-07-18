dcb0ff635f15e27276a1980cc2c9f1895c09897b
Kirsten Richard <36748441+krichard410@users.noreply.github.com>
2020-07-18 15:40:18 -0300

    Add Transactions instructions, clean up getting started page (#697)
    * Add Transactions instructions, clean up getting started page
    
    * Tiny tweaks
    
    * Update docs/getting-started.md
    
    Co-authored-by: Logan Saether <x@logansaether.com>
    
    * Update docs/getting-started.md
    
    Co-authored-by: Logan Saether <x@logansaether.com>
    
    * More feedback and CI Fix
    
    * Added in articles for beginners to read
    
    Co-authored-by: Logan Saether <x@logansaether.com>

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,6 +14,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    { doc: "getting-started", label: "Get Started" },
     { doc: "learn-introduction", label: "Learn" },
     { doc: "build-index", label: "Build" },
     { doc: "maintain-index", label: "Maintain " },
