0928a1637162806d72f5bcf9bb64b41a9eafd5d6
x@logansaether.com <x@logansaether.com>
2020-08-24 11:42:47 -0700

    Making the script output better

--- scripts/check-substrate-release.js
+++ scripts/check-substrate-release.js
@@ -3,42 +3,46 @@ const axios = require("axios");
 const fs = require("fs");
 
 (async () => {
-  try {
-    const ghApi = new Octokit();
-    const releases = await ghApi.repos.listReleases({
-      owner: "paritytech",
-      repo: "substrate",
-    });
-    let latestTag = releases.data[0].tag_name;
-    let pass = false;
-    while (!pass) {
-      let testUrl = `http://substrate.dev/rustdocs/${latestTag}/sc_service/index.html`;
-      try {
-        const res = await axios.get(testUrl);
-        if (res.status === 200) pass = true;
-      } catch (err) {
-        latestTag = latestTag.slice(0, -1);
-      }
+  const ghApi = new Octokit();
+  const releases = await ghApi.repos.listReleases({
+    owner: "paritytech",
+    repo: "substrate",
+  });
+  let latestTag = releases.data[0].tag_name;
+  let pass = false;
+  while (!pass) {
+    let testUrl = `http://substrate.dev/rustdocs/${latestTag}/sc_service/index.html`;
+    try {
+      const res = await axios.get(testUrl);
+      if (res.status === 200) pass = true;
+    } catch (err) {
+      latestTag = latestTag.slice(0, -1);
     }
+  }
 
-    console.log("Latest tag:", latestTag);
-    // Now read through all files.
-    const dir = fs.readdirSync("docs");
-    for (const file of dir) {
-      if (!file.endsWith(".md")) continue;
+  console.log("Latest tag:", latestTag);
+  // Now read through all files.
+  const dir = fs.readdirSync("docs");
+  const foundFiles = [];
+  for (const file of dir) {
+    if (!file.endsWith(".md")) continue;
 
-      const readFile = fs.readFileSync(`docs/${file}`, { encoding: "utf-8" });
-      for (const line of readFile.split("\n")) {
-        if (line.indexOf("substrate.dev/rustdocs") !== -1) {
-          if (line.indexOf(latestTag) === -1) {
-            console.log(line);
-            throw Error(`${file} does not have the latest tag`);
-          }
+    const readFile = fs.readFileSync(`docs/${file}`, { encoding: "utf-8" });
+    for (const line of readFile.split("\n")) {
+      if (line.indexOf("substrate.dev/rustdocs") !== -1) {
+        if (line.indexOf(latestTag) === -1) {
+          // console.log(line);
+          // throw Error(`${file} does not have the latest tag`);
+          foundFiles.push(file);
         }
       }
     }
-  } catch (err) {
-    console.error(err);
+  }
+
+  if (!!foundFiles.length) {
+    console.log("\nThe following files need to be updated for latest Substrate release:");
+    console.log(foundFiles.join("\n"));
+    console.log("\nPlease run `node scripts/update-substrate.js` to update them.");
     process.exit(1);
   }
 })();
