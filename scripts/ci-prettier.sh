c419a8c7b580c0b3de12db0425e7f0f796d8ad95
x@logansaether.com <x@logansaether.com>
2020-08-31 08:49:29 -0700

    Debug script

--- scripts/ci-prettier.sh
+++ scripts/ci-prettier.sh
@@ -6,5 +6,5 @@ if [ $? -eq 1] then
     git config --global user.name "Polkadot Wiki CI"
     echo "machine github.com login w3fbot password $GITHUB_TOKEN" > ~/.netrc
     node_modules/.bin/prettier -c docs/*.md --write
-    git add -A; git commit -m "Ran prettier"; git push
+    git add -A; git commit -m "Ran prettier"; git push;
 fi
