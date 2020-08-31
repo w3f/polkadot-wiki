ed8e7c0f453a9c0ddcd8bf575ae8fbfd9e1ed2c9
x@logansaether.com <x@logansaether.com>
2020-08-31 10:10:19 -0700

    Remove debug stuff

--- scripts/ci-prettier.sh
+++ scripts/ci-prettier.sh
@@ -8,8 +8,6 @@ then
     echo "machine github.com login w3fbot password $GITHUB_BOT_TOKEN" > ~/.netrc
     node_modules/.bin/prettier -c docs/*.md --write
     git remote set-url origin https://github.com/w3f/polkadot-wiki.git
-    ssh-add -l
-    ls ~/.ssh
     ssh-add -D
     ssh-add ~/.ssh/id_rsa_93218110749961b89a5041c875703b1d
     git add -A; git commit -m "Ran prettier"; git push -u origin HEAD;
