43dc8f8bbd4cabd3a05dc13c9553c1f854ae639f
x@logansaether.com <x@logansaether.com>
2020-08-31 09:40:34 -0700

    Fix bot token

--- scripts/ci-prettier.sh
+++ scripts/ci-prettier.sh
@@ -5,7 +5,7 @@ if [ $? -eq 1 ]
 then
     git config --global user.email "polkadot-wiki-deploy@users.noreply.github.com"
     git config --global user.name "Polkadot Wiki CI"
-    echo "machine github.com login w3fbot password $GITHUB_TOKEN" > ~/.netrc
+    echo "machine github.com login w3fbot password $GITHUB_BOT_TOKEN" > ~/.netrc
     node_modules/.bin/prettier -c docs/*.md --write
     git add -A; git commit -m "Ran prettier"; git push -u origin HEAD;
 fi
