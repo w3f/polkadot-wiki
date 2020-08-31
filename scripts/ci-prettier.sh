463706e45e5f01d912af82b155241d7c73fa1563
x@logansaether.com <x@logansaether.com>
2020-08-31 08:46:18 -0700

    Modify CI to run prettier

---
+++ scripts/ci-prettier.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+node_modules/.bin/prettier -c docs/*.md
+if [ $? -eq 1] then
+    git config --global user.email "polkadot-wiki-deploy@users.noreply.github.com"
+    git config --global user.name "Polkadot Wiki CI"
+    echo "machine github.com login w3fbot password $GITHUB_TOKEN" > ~/.netrc
+    node_modules/.bin/prettier -c docs/*.md --write
+    git add -A; git commit -m "Ran prettier"; git push
+fi
