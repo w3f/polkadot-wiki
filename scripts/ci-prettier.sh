60ce4957e6d2d51210543f8592173e23b7086ae1
x@logansaether.com <x@logansaether.com>
2020-08-31 10:06:38 -0700

    Add correct key

--- scripts/ci-prettier.sh
+++ scripts/ci-prettier.sh
@@ -10,5 +10,7 @@ then
     git remote set-url origin https://github.com/w3f/polkadot-wiki.git
     ssh-add -l
     ls ~/.ssh
+    ssh-add -D
+    ssh-add ~/.ssh/id_rsa_93218110749961b89a5041c875703b1d
     git add -A; git commit -m "Ran prettier"; git push -u origin HEAD;
 fi
