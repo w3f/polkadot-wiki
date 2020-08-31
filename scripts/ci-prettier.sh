035023acbe27df8423fdd9bb8bedf47003a034fd
x@logansaether.com <x@logansaether.com>
2020-08-31 09:02:25 -0700

    Debug script

--- scripts/ci-prettier.sh
+++ scripts/ci-prettier.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 node_modules/.bin/prettier -c docs/*.md
-if [ $? -eq 1 ] then
+if [ $? -eq 1 ]
+then
     git config --global user.email "polkadot-wiki-deploy@users.noreply.github.com"
     git config --global user.name "Polkadot Wiki CI"
     echo "machine github.com login w3fbot password $GITHUB_TOKEN" > ~/.netrc
