1d873abb4c5c64a9284b621a415464bb75f30616
Bruno Škvorc <bruno@skvorc.me>
2020-09-22 09:48:04 +0200

    Added ENS guide (#973)
    * Added ENS guide
    
    * Update docs/ens.md
    
    Co-authored-by: Logan Saether <x@logansaether.com>
    
    * Update docs/ens.md
    
    Co-authored-by: Logan Saether <x@logansaether.com>
    
    * Addressed feedback
    
    * Gas limit note change
    
    * Added YT link
    
    * Update docs/ens.md
    
    Co-authored-by: Kirsten Richard <36748441+krichard410@users.noreply.github.com>
    
    * Ran prettier
    
    * Clarified wallets recommendation
    
    * Update docs/ens.md
    
    Co-authored-by: Bill Laboon <laboon@users.noreply.github.com>
    
    * Update docs/ens.md
    
    Co-authored-by: Bill Laboon <laboon@users.noreply.github.com>
    
    * Update docs/ens.md
    
    Co-authored-by: Bill Laboon <laboon@users.noreply.github.com>
    
    * Addressed feedback
    
    Co-authored-by: Logan Saether <x@logansaether.com>
    Co-authored-by: Kirsten Richard <36748441+krichard410@users.noreply.github.com>
    Co-authored-by: Polkadot Wiki CI <polkadot-wiki-deploy@users.noreply.github.com>
    Co-authored-by: Bill Laboon <laboon@users.noreply.github.com>

--- scripts/mirror.js
+++ scripts/mirror.js
@@ -3,52 +3,55 @@
  * that we can "mirror" the same document between the two. However, due to
  * a peculiarity in Docusaurus, when we put the same document into both sub-wikis
  * in `website/sidebars.json` only the most recent entry is read.
- * 
+ *
  * To solve this, we create an explicit mirror of the document. The process for
  * adding content to both wikis is as follows:
- * 
- * 1) Create the source document and put it under the relevant sidebar in the 
+ *
+ * 1) Create the source document and put it under the relevant sidebar in the
  *    Polkadot wiki configuration in `sidebars.json`.
- * 2) Add the source document's name to the `mirrored` array below. 
+ * 2) Add the source document's name to the `mirrored` array below.
  * 3) Run `node mirror.js`.
  * 4) Add `mirror-DOCUMENT_NAME` to the sidebar for Kusama in `sidebars.json`.
- * 
+ *
  */
 
-const fs = require('fs');
+const fs = require("fs");
 
 // List of mirrored files that should be in both the Polkadot wiki and the Kusama
 // user guide.
 const mirrored = [
-  'build-build-with-polkadot',
-  'learn-identity',
-  'learn-governance',
-  'learn-treasury',
-  'maintain-guides-how-to-nominate-kusama',
-  'maintain-guides-how-to-validate-kusama',
-  'maintain-errors'
+  "build-build-with-polkadot",
+  "learn-identity",
+  "ens",
+  "learn-governance",
+  "learn-treasury",
+  "maintain-guides-how-to-nominate-kusama",
+  "maintain-guides-how-to-validate-kusama",
+  "maintain-errors",
 ];
 
 for (const file of mirrored) {
-  const doc = './docs/' + file + '.md';
-  const mirror = './docs/mirror-' + file + '.md';
+  const doc = "./docs/" + file + ".md";
+  const mirror = "./docs/mirror-" + file + ".md";
   if (!fs.existsSync(doc)) {
     throw new Error(`${doc} doesn't exist!`);
   }
 
-  const content = fs.readFileSync(doc, { encoding: 'utf-8' });
-  const mirroredContent = content.split('\n').map((line) => {
-    if (line.startsWith('id:')) {
-      const [before, after] = line.split(' ');
-      return before + ' mirror-' + after;
-    }
-    else return line;
-  }).join('\n');
+  const content = fs.readFileSync(doc, { encoding: "utf-8" });
+  const mirroredContent = content
+    .split("\n")
+    .map((line) => {
+      if (line.startsWith("id:")) {
+        const [before, after] = line.split(" ");
+        return before + " mirror-" + after;
+      } else return line;
+    })
+    .join("\n");
 
   fs.writeFileSync(mirror, mirroredContent);
 }
 
-const langDirectories = fs.readdirSync('./website/translated_docs');
+const langDirectories = fs.readdirSync("./website/translated_docs");
 for (const lang of langDirectories) {
   for (const file of mirrored) {
     const doc = `./website/translated_docs/${lang}/${file}.md`;
@@ -58,13 +61,16 @@ for (const lang of langDirectories) {
       throw new Error(`${doc} doesn't exist!`);
     }
 
-    const content = fs.readFileSync(doc, { encoding: 'utf-8' });
-    const mirroredContent = content.split('\n').map((line) => {
-      if (line.startsWith('id:')) {
-        const [before,after] = line.split(' ');
-        return `${before} mirror-${after}`;
-      } else return line;
-    }).join('\n');
+    const content = fs.readFileSync(doc, { encoding: "utf-8" });
+    const mirroredContent = content
+      .split("\n")
+      .map((line) => {
+        if (line.startsWith("id:")) {
+          const [before, after] = line.split(" ");
+          return `${before} mirror-${after}`;
+        } else return line;
+      })
+      .join("\n");
 
     fs.writeFileSync(mirror, mirroredContent);
   }
