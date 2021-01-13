aae50224174f15d8c4c7a06afc4f1b398e6ef05e
Logan Saether <X@logansaether.com>
2021-01-13 13:45:29 +0100

    Add header text

--- scripts/genereateRegistryTable.js
+++ scripts/genereateRegistryTable.js
@@ -3,8 +3,13 @@ const axios = require("axios");
 const registryLink = "https://raw.githubusercontent.com/paritytech/substrate/master/ss58-registry.json";
 
 let output = `
+---
+id: build-ss58-registry
+title: SS58 Registry
+sidebar_label: SS58 Registry
+---
 
-## Prefix Registry Table
+Substrate-based chains, including the Polkadot and Kusama relay chains, use an [SS58 encoding](https://github.com/paritytech/substrate/wiki/External-Address-Format-(SS58)) for their address formats. This page serves as a registry for teams to see which chain corresponds to a given prefix, and which prefixes are availabe.
 
 | Prefix | Network | Name | Symbols | Decimals | Standard Account | Website |
 | - | - | - | - | - | - | - |`;
