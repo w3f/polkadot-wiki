506b3fff0237750a071a95234809a3bb706b7426
Bruno Škvorc <bruno@skvorc.me>
2021-06-22 16:37:27 +0200

    Update docusaurus.config.js

--- website-v2/docusaurus.config.js
+++ website-v2/docusaurus.config.js
@@ -10,7 +10,7 @@ module.exports = {
   tagline: "The hub for those interested in learning, building, or running a node on Polkadot.",
   titleDelimiter: "·",
   url: "https://wiki.polkadot.network",
-  baseUrl: "/en/",
+  baseUrl: "/",
   projectName: "polkadot-wiki",
   organizationName: "w3f",
   scripts: [
