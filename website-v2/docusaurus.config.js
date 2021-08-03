bd628810d9eaebc81339ff204fb78843e6d5c68a
Radha <86818441+DrW3RK@users.noreply.github.com>
2021-08-03 19:31:26 +0200

    Update learn-accounts.md (#2416)
    * Update learn-accounts.md
    
    Added placeholder for address changer. Possibly a breaking change due to upgrade
    
    * Update docusaurus.config.js
    
    Adding a redirect to check if it fixes js issues

--- website-v2/docusaurus.config.js
+++ website-v2/docusaurus.config.js
@@ -87,6 +87,7 @@ module.exports = {
               existingPath.replace("/docs/", "/docs/en/"),
               existingPath.replace("/docs/", "/docs/zh-CN/"),
               existingPath.replace("/docs/", "/docs/ru-RU/"),
+              existingPath.replace("/js/", "/docs/js/"),
             ];
           }
 
