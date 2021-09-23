e403d5bd08bf0940880d6f5acba016a4980fc664
Danny Salman <salman.danny03@hotmail.com>
2021-09-22 23:16:17 -0400

    Fix Kusama Guide Main Page for Mobile (#2631)
    * init kusama guide landing page update
    
    * update main page for kusama guide; index.js, footer.js & sections
    
    * edits
    
    * update css to fix mobile ccompatibility
    
    * edits
    
    * update css to fix mobile ccompatibility
    
    * update css to fix mobile ccompatibility
    
    * update css to fix mobile ccompatibility

--- kusama-guide/src/pages/index.js
+++ kusama-guide/src/pages/index.js
@@ -12,14 +12,16 @@ function HomeSplash() {
 
   return (
     <SplashContainer>
-      <button className="kusama-mainpage-build-btn-main homepage-font">
-        <a
-          className="homepage-font-start"
-          href="https://guide.kusama.network/docs/kusama-getting-started"
-        >
-          GET STARTED WITH KUSAMA
-        </a>
-      </button>
+      <div className="mainContainer" style={{ padding: "0" }}>
+        <button className="kusama-mainpage-build-btn-main homepage-font">
+          <a
+            className="homepage-font-start"
+            href="https://guide.kusama.network/docs/kusama-getting-started"
+          >
+            GET STARTED WITH KUSAMA
+          </a>
+        </button>
+      </div>
     </SplashContainer>
   );
 }
@@ -72,7 +74,7 @@ export default function Index() {
             </div>
             <div className="mainpage-column">
               <h2 className="homepage-font">Become a Validator</h2>
-              <p className="homepage-font-right">
+              <p className="homepage-font">
                 The minimum requirement for staking as a validator on Kusama is
                 much lower than it is expected for Polkadot. There are also
                 programs in place such as{" "}
