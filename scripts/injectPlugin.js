b4f240032352cb515818c84bb185700b53d648f2
Radha <86818441+DrW3RK@users.noreply.github.com>
2021-09-28 01:09:51 +0200

    Update injectPlugin.js
    revert

--- scripts/injectPlugin.js
+++ scripts/injectPlugin.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-import {visit} from 'unist-util-visit';
+const visit = require("unist-util-visit");
 const { yellow, cyan } = require("chalk");
 
 const R_POLKADOT = /{{ polkadot: ([\s\S]+?) :polkadot }}/gim;
