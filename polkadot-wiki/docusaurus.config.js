8e00a234503faf66cad3cf7e23f8a90ae9572aed
Radha <86818441+DrW3RK@users.noreply.github.com>
2021-09-28 01:26:47 +0200

    Remove usage of injectPlugin.js to fix Wiki build error (#2661)
    * Update injectPlugin.js
    
    Testing out the instructions given in https://www.npmjs.com/package/unist-util-visit
    
    * Update injectPlugin.js
    
    * Update injectPlugin.js
    
    revert
    
    * Update docusaurus.config.js
    
    Checking the origin of the issue
    
    * Update docusaurus.config.js

--- polkadot-wiki/docusaurus.config.js
+++ polkadot-wiki/docusaurus.config.js
@@ -1,5 +1,4 @@
 const { baseUrlPattern } = require("../scripts/utils");
-const { injectPlugin } = require("../scripts/injectPlugin");
 const i18n = require("./i18n");
 
 const isBuilding = process.env.BUILDING === "true";
@@ -58,7 +57,6 @@ module.exports = {
           showLastUpdateTime: true,
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "docs",
-          remarkPlugins: [injectPlugin({ isPolkadot: true })],
         },
         theme: {
           customCss: [
