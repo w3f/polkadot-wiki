5f807dc1c66ad0c78f58727c29455d510913876a
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-10-27 11:21:12 -0400

    ipfs CICD rc

--- scripts/ipfs-cluster-pin.js
+++ scripts/ipfs-cluster-pin.js
@@ -31,7 +31,7 @@ const main = async () => {
     `ipfs-cluster-ctl \
     --enc json \
     --basic-auth ${argv.auth} \
-    --host /dns4/hemmerle.dev/tcp/9097/p2p/12D3KooWBxH7hKkvvp2oCDay9jSh8gw5FkdBsdgPytXFjRgqDvf8 \
+    --host /dns4/ipfs.w3f.community/tcp/9096/p2p/12D3KooWMfXzp2nmNrb7DM4PETYZbaKALnrnwiqnhrvUC66KyYrb \
     ${command}`,
     { encoding: 'utf-8' });
 
