5e310a263ddbc76e91e9d89d0daddf5aee380d08
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-11-23 19:15:46 -0500

    test

--- scripts/ipfs-cluster-pin.js
+++ scripts/ipfs-cluster-pin.js
@@ -2,42 +2,51 @@ const yargs = require("yargs");
 const { create, globSource } = require("ipfs-http-client");
 const { execSync } = require("child_process");
 
-const argv = yargs(process.argv)
-  .option("websiteDir", {
-    alias: "d",
-    description: "root directory to find files",
-    type: "string",
-  })
-  .option("pinName", {
-    alias: "n",
-    description: "required. eg. polkadot-wiki",
-    type: "string",
-  })
-  .option("auth", {
-    alias: "a",
-    description: "required. eg. asdf:1234",
-    type: "string",
-  })
-  .help()
-  .alias("help", "h").argv;
+// const argv = yargs(process.argv)
+//   .option("websiteDir", {
+//     alias: "d",
+//     description: "root directory to find files",
+//     type: "string",
+//   })
+//   .option("pinName", {
+//     alias: "n",
+//     description: "required. eg. polkadot-wiki",
+//     type: "string",
+//   })
+//   .option("auth", {
+//     alias: "a",
+//     description: "required. eg. asdf:1234",
+//     type: "string",
+//   })
+//   .help()
+//   .alias("help", "h").argv;
 
-if (!argv.websiteDir || !argv.pinName || !argv.auth) {
-  throw new Error("Must pass --websiteDir, --auth, and --pinName arguments.");
-}
+// if (!argv.websiteDir || !argv.pinName || !argv.auth) {
+//   throw new Error("Must pass --websiteDir, --auth, and --pinName arguments.");
+// }
 
-const main = async () => {
-  // js-cluster-api doesn't support libp2p so we have to use go shell version...
-  const runCommandOnCluster = async (command) => execSync(
+let runCommandOnCluster = async (command, retries = 3) => {
+  const clusterCommander = async (command) => execSync(
     `ipfs-cluster-ctl \
     --enc json \
     --basic-auth ${argv.auth} \
     --host /dns4/ipfs.w3f.community/tcp/9096/p2p/12D3KooWMfXzp2nmNrb7DM4PETYZbaKALnrnwiqnhvrUC66KyYrb \
     ${command} \n`,
     { encoding: 'utf-8' });
+  
+  return clusterCommander(command).then(
+    (result) => JSON.parse(result),
+    (error) => {
+      return retries ? runCommandOnCluster(command, retries - 1) : error;
+    });
+}
+
+const main = async () => {
+  // js-cluster-api doesn't support libp2p so we have to use compiled version...
 
   // return CID as string from a named pin on the cluster
   const getCidByPinName = async (pinName) => {
-    const pins = JSON.parse(await runCommandOnCluster('status'));
+    const pins = await runCommandOnCluster('status');
     const prevCidObject = pins.find(pin => pin.name == pinName);
     return prevCidObject ? prevCidObject.cid["/"] : null;
   }
@@ -56,6 +65,8 @@ const main = async () => {
   await runCommandOnCluster(`add -r --name ${argv.pinName} ${argv.websiteDir}`);
   const cid = await getCidByPinName(argv.pinName);
   console.log(`Successfully added and pinned '${argv.pinName}' (CID ${cid}).`);
+
+  // update DNS entry
 };
 
 try {
