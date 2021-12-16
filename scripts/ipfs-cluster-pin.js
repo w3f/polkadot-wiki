3a5633f9b0e720bad55ebb6c6ab5878424c05857
Jake Hemmerle <jakehemmerle@protonmail.com>
2021-12-15 22:02:10 -0500

    cicd: added cloudflare dns txt support for dnslink. NOT tested

--- scripts/ipfs-cluster-pin.js
+++ scripts/ipfs-cluster-pin.js
@@ -1,68 +1,124 @@
-const yargs = require("yargs");
-const { create, globSource } = require("ipfs-http-client");
-const { execSync } = require("child_process");
+const yargs = require('yargs');
+const { create, globSource } = require('ipfs-http-client');
+const { execSync } = require('child_process');
+const cloudflare = require('cloudflare');
 
 const argv = yargs(process.argv)
-  .option("websiteDir", {
-    alias: "d",
-    description: "root directory to find files",
-    type: "string",
+  .option('websiteDir', {
+    alias: 'd',
+    description: 'root directory to find files',
+    type: 'string',
   })
-  .option("pinName", {
-    alias: "n",
-    description: "required. eg. polkadot-wiki",
-    type: "string",
+  .option('pinName', {
+    alias: 'n',
+    description: 'required. eg. polkadot-wiki',
+    type: 'string',
   })
-  .option("auth", {
-    alias: "a",
-    description: "required. eg. asdf:1234",
-    type: "string",
+  .option('auth', {
+    alias: 'a',
+    description: 'required. eg. asdf:1234',
+    type: 'string',
+  })
+  .option('cloudflareToken', {
+    alias: 't',
+    description: 'required',
+    type: 'string',
   })
   .help()
-  .alias("help", "h").argv;
+  .alias('help', 'h').argv;
 
-if (!argv.websiteDir || !argv.pinName || !argv.auth) {
-  throw new Error("Must pass --websiteDir, --auth, and --pinName arguments.");
+if (!argv.websiteDir || !argv.pinName || !argv.auth || !argv.cloudflareToken) {
+  throw new Error('Must pass --websiteDir, --auth, --cloudflareToken, and --pinName arguments.');
 }
 
 var runCommandOnCluster = async (command, retries = 6) => {
   // Note: js-cluster-api doesn't support libp2p so we have to use it via the compiled Go version via shell
-  const clusterCommander = async (command) => execSync(
-    `ipfs-cluster-ctl \
+  const clusterCommander = async (command) =>
+    execSync(
+      `ipfs-cluster-ctl \
     --enc json \
     --basic-auth ${argv.auth} \
     --host /dns4/ipfs.w3f.community/tcp/9096/p2p/12D3KooWMfXzp2nmNrb7DM4PETYZbaKALnrnwiqnhvrUC66KyYrb \
     ${command} \n`,
-    { encoding: 'utf-8' });
-  
+      { encoding: 'utf-8' }
+    );
+
   return clusterCommander(command).then(
     (result) => {
       try {
         return JSON.parse(result);
       } catch (err) {
         // https://github.com/ipfs/ipfs-cluster/issues/1365
-        console.log("Could not parse IPFS Cluster JSON response. This is not fatal.");
+        console.log('Could not parse IPFS Cluster JSON response. This is not fatal.');
         return null;
       }
     },
     (error) => {
       if (retries > 0) {
-        console.log("Retrying cluster command...");
+        console.log('Retrying cluster command...');
         return runCommandOnCluster(command, retries - 1);
       } else {
         return error;
       }
-    });
-}
+    }
+  );
+};
 
 // return CID as string from a named pin on the cluster
 const getCidByPinName = async (pinName) => {
   const pins = await runCommandOnCluster('status');
-  const prevCidObject = pins.find(pin => pin.name == pinName);
-  return prevCidObject ? prevCidObject.cid["/"] : null;
+  const prevCidObject = pins.find((pin) => pin.name == pinName);
+  return prevCidObject ? prevCidObject.cid['/'] : null;
+};
+
+class CFClient {
+  constructor(pinName, apiToken) {
+    this.cf = cloudflare({
+      email: 'jake@web3.foundation',
+      key: apiToken,
+    });
+
+    this.zone = this._getZoneFromPinName(pinName);
+  }
+
+  static _getZoneFromPinName(pinName) {
+    const zones = {
+      'polkadot-wiki': 'polkadot-wiki.w3f.community',
+      'kusama-guide': 'kusama-guide.w3f.community',
+    };
+
+    return zones[pinName];
+  }
+
+  async updateDNSLinkFromCID(newMultiaddr) {
+    const record = {
+      type: 'TXT',
+      name: this.zone,
+      content: `dnslink=/ipfs/${newMultiaddr}`,
+      ttl: 120, // seconds
+    };
+
+    try {
+      return this.cf.dnsRecords.edit({
+        zone_id: argv.zoneId,
+        id: argv.id,
+        record,
+      });
+    } catch (e) {
+      console.log('DNS Record does not exist:');
+      console.log(record);
+      console.log('Adding...');
+
+      return this.cf.dnsRecords.add({
+        zone_id: argv.zoneId,
+        record,
+      });
+    }
+  }
 }
 
 const main = async () => {
+  const cf = CFClient(argv.pinName, argv.cloudflareToken); // we can derive which dns zone to use based on the pinName
   // get and unpin the previous build from the cluster, if exists
   const prevCid = await getCidByPinName(argv.pinName);
   if (prevCid) {
@@ -71,15 +127,19 @@ const main = async () => {
   } else {
     console.log(`No existing pin named '${argv.pinName}'.`);
   }
-  
+
   // pin new build to cluster with new name
-  console.log(`Uploading and pinning '${argv.websiteDir}' to the cluster as '${argv.pinName}'...`)
+  console.log(`Uploading and pinning '${argv.websiteDir}' to the cluster as '${argv.pinName}'...`);
   await runCommandOnCluster(`add -r --name ${argv.pinName} ${argv.websiteDir}`);
   const cid = await getCidByPinName(argv.pinName);
   console.log(`Successfully added and pinned '${argv.pinName}' (CID ${cid}).`);
-  console.log("Previous errors are non-fatal.")
+  console.log('If above values are non-null, previous errors are non-fatal.');
+
+  await cf.updateDNSLinkFromCID(cid); // update the
 
   // update DNS entry
+  // https://api.cloudflare.com/#dns-records-for-a-zone-update-dns-record
+  // https://github.com/cloudflare/node-cloudflare
 };
 
 try {
