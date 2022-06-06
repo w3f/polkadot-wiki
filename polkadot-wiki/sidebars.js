7f45752cf1c0975863d9b8a7f4e67581eef2f263
emresurmeli <s.emre.s.8@gmail.com>
2022-06-06 15:51:16 +0100

    Revert "Add avalanche page to sidebars"
    This reverts commit ea40642ca0eab2ce1a026c8975864175c941c291.

--- polkadot-wiki/sidebars.js
+++ polkadot-wiki/sidebars.js
@@ -140,7 +140,6 @@ module.exports = {
             'learn/learn-comparisons',
             'learn/learn-comparisons-ethereum-2',
             'learn/learn-comparisons-cosmos',
-            'learn/learn-comparisons-avalanche'
           ],
         },
         'learn/learn-launch',
