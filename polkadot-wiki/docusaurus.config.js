7bed0d689c61a5de314fb018573883e48f928169
Radha <86818441+DrW3RK@users.noreply.github.com>
2022-07-01 11:46:24 +0200

    comment out announcement bars

--- polkadot-wiki/docusaurus.config.js
+++ polkadot-wiki/docusaurus.config.js
@@ -96,6 +96,7 @@ module.exports = {
     prism: {
       theme: require('prism-react-renderer/themes/github'),
     },
+    /*
     announcementBar: {
       id: 'announcement',
       content: '<a target="_blank" href="https://decoded.polkadot.network/">POLKADOT DECODED June 29th-30th, 2022. Join 100+ talks, live streamed from 4 locations worldwide</a>',
@@ -103,6 +104,7 @@ module.exports = {
       textColor: '#FFFFFF',
       isCloseable: false,
     },
+    */
     liveCodeBlock: {
       /**
        * The position of the live playground, above or under the editor
