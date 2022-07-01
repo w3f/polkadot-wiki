7fd16acfe1854426304e45a01d41ecba674f9fb9
Radha <86818441+DrW3RK@users.noreply.github.com>
2022-07-01 11:39:15 +0200

    comment out banner - polkadot wiki

--- polkadot-wiki/src/pages/index.js
+++ polkadot-wiki/src/pages/index.js
@@ -256,7 +256,7 @@ export default function Index() {
       <div className="homeContainer">
         <HomeNav />
         <HomeFooter />
-        <Banner />
+        <! --  <Banner /> --> 
       </div>
     </Layout>
   );
