a173ed2958f5591ba1d310539d574cf018bc2669
alfarok <keith4895@gmail.com>
2022-07-07 11:47:57 -0400

    consolidate mirror logic and append mirror- to slugs

--- scripts/mirror.mjs
+++ scripts/mirror.mjs
@@ -1,163 +1,106 @@
-import fs from "fs";
+import { existsSync, readFileSync, writeFileSync} from "fs";
 
-/**
- * The wiki hosts both the Polkadot Wiki and the Kusama User Guide. This means
- * that we can "mirror" the same document between the two. However, due to
- * a peculiarity in Docusaurus, when we put the same document into both sub-wikis
- * in `website/sidebars.json` only the most recent entry is read.
- *
- * To solve this, we create an explicit mirror of the document. The process for
- * adding content to both wikis is as follows:
- *
- * 1) Create the source document and put it under the relevant sidebar in the
- *    Polkadot wiki configuration in `sidebars.json`.
- * 2) Add the source document's name to the `mirrored` array below.
- * 3) Run `node mirror.js`.
- * 4) Add `mirror-DOCUMENT_NAME` to the sidebar for Kusama in `sidebars.json`.
- *
+/*
+  The wiki hosts both the Polkadot Wiki and the Kusama User Guide. This means
+  that we can "mirror" the same document between the two. However, due to
+  a peculiarity in Docusaurus, when we put the same document into both sub-wikis
+  in `website/sidebars.json` only the most recent entry is read.
+
+  To solve this, we create an explicit mirror of the document. The process for
+  adding content to both wikis is as follows:
+ 
+  1) Create the source document and put it under the relevant sidebar in the
+     Polkadot wiki configuration in `sidebars.json`.
+  2) Add the source document's name to the `mirrored` array below.
+  3) Run `node mirror.js` which duplicates the original markdown file and
+     appends 'mirror-' to the file's id and slug to prevent duplicate routes.
+  4) Add `mirror-DOCUMENT_NAME` to the sidebar for Kusama in `sidebars.json`.
  */
 
 // List of mirrored files that should be in both the Polkadot wiki and the Kusama
 // user guide.
 
-const mirrored_general = ["ens", "thousand-validators"];
-
-const mirrored_learn = [
-  "learn-identity",
-  "learn-balance-transfers",
-  "learn-governance",
-  "learn-treasury",
-  "learn-registrar",
-  "learn-auction",
-  "learn-parachains",
-  "learn-parathreads",
-  "learn-crowdloans",
-];
-
-const mirrored_build = [
-  "build-guide", 
-  "build-parachains",
-  "build-storage",
-  "build-smart-contracts",
-  "build-oracle",
-  "build-wallets",
-  "build-tools-index",
-  "build-hackathon",
-];
-
-const mirrored_maintain = [
-  "maintain-guides-how-to-stop-validating",
-  "maintain-errors",
-  "maintain-guides-validator-community",
-];
-
-const mirrored_maintain_kusama = [
-  "maintain-guides-how-to-nominate-kusama",
-  "maintain-guides-how-to-validate-kusama",
-];
-
-// Mirror general docs
-for (const file of mirrored_general) {
-  const doc = "./docs/general/" + file + ".md";
-  const mirror = "./docs/general/mirror-" + file + ".md";
-  if (!fs.existsSync(doc)) {
-    throw new Error(`${doc} doesn't exist!`);
-  }
-
-  const content = fs.readFileSync(doc, "utf8");
-  const mirroredContent = content
-    .split("\n")
-    .map((line) => {
-      if (line.startsWith("id:")) {
-        const [before, after] = line.split(" ");
-        return before + " mirror-" + after;
-      } else return line;
-    })
-    .join("\n");
-
-  fs.writeFileSync(mirror, mirroredContent);
+const mirroredDocs = {
+  general: [
+    "ens",
+    "thousand-validators"
+  ],
+  learn: [
+    "learn-identity",
+    "learn-balance-transfers",
+    "learn-governance",
+    "learn-treasury",
+    "learn-registrar",
+    "learn-auction",
+    "learn-parachains",
+    "learn-parathreads",
+    "learn-crowdloans",
+  ],
+  build: [
+    "build-guide", 
+    "build-parachains",
+    "build-storage",
+    "build-smart-contracts",
+    "build-oracle",
+    "build-wallets",
+    "build-tools-index",
+    "build-hackathon",
+  ],
+  maintain: [
+    "maintain-guides-how-to-stop-validating",
+    "maintain-errors",
+    "maintain-guides-validator-community",
+  ],
+  "maintain/kusama": [
+    "maintain-guides-how-to-nominate-kusama",
+    "maintain-guides-how-to-validate-kusama",
+  ]
 }
-// Mirror learn docs
-for (const file of mirrored_learn) {
-  const doc = "./docs/learn/" + file + ".md";
-  const mirror = "./docs/learn/mirror-" + file + ".md";
-  if (!fs.existsSync(doc)) {
-    throw new Error(`${doc} doesn't exist!`);
-  }
 
-  const content = fs.readFileSync(doc, "utf8");
-  const mirroredContent = content
-    .split("\n")
-    .map((line) => {
-      if (line.startsWith("id:")) {
-        const [before, after] = line.split(" ");
-        return before + " mirror-" + after;
-      } else return line;
-    })
-    .join("\n");
-
-  fs.writeFileSync(mirror, mirroredContent);
+for (const [key, value] of Object.entries(mirroredDocs)) {
+  buildMirrors(key, value)
 }
-// Mirror build docs
-for (const file of mirrored_build) {
-  const doc = "./docs/build/" + file + ".md";
-  const mirror = "./docs/build/mirror-" + file + ".md";
-  if (!fs.existsSync(doc)) {
-    throw new Error(`${doc} doesn't exist!`);
-  }
 
-  const content = fs.readFileSync(doc, "utf8");
-  const mirroredContent = content
-    .split("\n")
-    .map((line) => {
-      if (line.startsWith("id:")) {
-        const [before, after] = line.split(" ");
-        return before + " mirror-" + after;
-      } else return line;
-    })
-    .join("\n");
+function buildMirrors(dir, fileNames) {
+  fileNames.forEach(file => {
+    // Define origin and mirror paths
+    const doc = `./docs/${dir}/${file}.md`;
+    const mirror = `./docs/${dir}/mirror-${file}.md`;
+    
+    // Make sure origin file exists
+    if (!existsSync(doc)) {
+      throw new Error(`${doc} doesn't exist!`);
+    }
 
-  fs.writeFileSync(mirror, mirroredContent);
-}
-// Mirror maintain docs
-for (const file of mirrored_maintain) {
-  const doc = "./docs/maintain/" + file + ".md";
-  const mirror = "./docs/maintain/mirror-" + file + ".md";
-  if (!fs.existsSync(doc)) {
-    throw new Error(`${doc} doesn't exist!`);
-  }
+    // Read origin
+    const content = readFileSync(doc, "utf8");
 
-  const content = fs.readFileSync(doc, "utf8");
-  const mirroredContent = content
-    .split("\n")
-    .map((line) => {
-      if (line.startsWith("id:")) {
-        const [before, after] = line.split(" ");
-        return before + " mirror-" + after;
-      } else return line;
-    })
-    .join("\n");
-
-  fs.writeFileSync(mirror, mirroredContent);
-}
-// Mirror kusama maintain docs
-for (const file of mirrored_maintain_kusama) {
-  const doc = "./docs/maintain/kusama/" + file + ".md";
-  const mirror = "./docs/maintain/kusama/mirror-" + file + ".md";
-  if (!fs.existsSync(doc)) {
-    throw new Error(`${doc} doesn't exist!`);
-  }
-
-  const content = fs.readFileSync(doc, "utf8");
-  const mirroredContent = content
-    .split("\n")
-    .map((line) => {
-      if (line.startsWith("id:")) {
-        const [before, after] = line.split(" ");
-        return before + " mirror-" + after;
-      } else return line;
-    })
-    .join("\n");
-
-  fs.writeFileSync(mirror, mirroredContent);
+    // Modify content
+    const mirroredContent = content
+      .split("\n")
+      .map((line) => {
+        // Append mirror- to id
+        if (line.startsWith("id:")) {
+          const [before, after] = line.split(" ");
+          return `${before} mirror-${after}`;
+        }
+        // Append mirror- to slug
+        else if (line.startsWith("slug:")) {
+          let route = line.split("/");
+          const trailingRoute = route[route.length - 1];
+          const trailingMirrorRoute = `mirror-${trailingRoute}`;
+          route.pop();
+          route.push(trailingMirrorRoute);
+          const slug = route.join('/');
+          return slug;
+        } else {
+          // Keep line as-is
+          return line;
+        }
+      })
+      .join("\n");
+    
+    // Write mirror
+    writeFileSync(mirror, mirroredContent);
+  });
 }
