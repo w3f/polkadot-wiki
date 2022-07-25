32cd794eacc8e7c079d4c5dc751057ff86914e20
Radha <86818441+DrW3RK@users.noreply.github.com>
2022-07-25 17:45:15 +0200

    Remove ambiguity with precision (#3567)
    * Remove ambiguity with precision
    
    * Update components/RPC-Connection.jsx
    
    Co-authored-by: Keith Alfaro <keith4895@gmail.com>
    
    * Update components/RPC-Connection.jsx
    
    Co-authored-by: Keith Alfaro <keith4895@gmail.com>
    
    Co-authored-by: Keith Alfaro <keith4895@gmail.com>

--- components/RPC-Connection.jsx
+++ components/RPC-Connection.jsx
@@ -157,9 +157,14 @@ function applyFilter(value, filter, network, setReturnValue) {
 			}
 			// String to number
 			value = parseFloat(value);
-			// TODO: this legacy function needs some attention (see https://github.com/w3f/polkadot-wiki/issues/3539)
 			// Apply precision
-    		value = `${(value / values[network].precision).toFixed(decimals)} ${values[network].symbol}`;
+			if (Number.isInteger(value / values[network].precision)) {
+				value = `${value / values[network].precision} ${values[network].symbol}`;
+			} else {
+				value = `${(value / values[network].precision).toFixed(decimals)} ${values[network].symbol}`;
+			}
+
+    		
     		break;
 		case "blocksToDays":
 			value = (value * 6) / 86400;
