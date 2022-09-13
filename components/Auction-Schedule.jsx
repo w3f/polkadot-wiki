f6111afc714b9d51f65cd0269f5b61bc7bf06d57
alfarok <keith4895@gmail.com>
2022-09-13 17:08:43 -0400

    update abstraction to handle DOT and KSM

--- components/Auction-Schedule.jsx
+++ components/Auction-Schedule.jsx
@@ -17,65 +17,17 @@ function AuctionSchedule() {
 
 	useEffect(async () => {
 		const title = document.title;
-		
+
 		// Polkadot
 		if (title === "Parachain Slot Auctions · Polkadot Wiki") {
 			// Set chain type
 			chain = "polkadot"
 			wsProvider = new WsProvider("wss://rpc.polkadot.io");
-  		api = await ApiPromise.create({ provider: wsProvider });
-
-			// Get the current block for projection
-			const currentBlock = await api.rpc.chain.getBlock();
-			const currentBlockNumber = parseInt(currentBlock.block.header.number.toString());
-
-			// Get current date/time
-			let date = new Date();
-
-			// Get ending period for the given chain
-			const endPeriod = parseInt(api.consts.auctions.endingPeriod.toString());
-			
-			// Add starting block for the given chain
-			let auctions = [];
-			let auctionBlocks = [];
-			auctionBlocks.push(firstAuctionBlockDot);
-
-			// Build auction objects with all required values for UI
-			for (let i = 0; i < auctionCount; i++) {
-				let auction = { };
-				auction.startBlock = auctionBlocks[auctionBlocks.length - 1];
-				auction.startHash = (await api.rpc.chain.getBlockHash(auction.startBlock)).toString();
-				const apiAt = await api.at(auction.startHash);
-				const [lease, end] = (await apiAt.query.auctions.auctionInfo()).toJSON();
-
-				auction.weeksLeased = parseInt(lease);
-				auction.endPeriodBlock = parseInt(end);
-				auction.biddingEndsBlock = auction.endPeriodBlock + endPeriod;
-				auction.startDate = EstimateBlockDate(date, currentBlockNumber, auction.startBlock);
-				auction.endPeriodDate = EstimateBlockDate(date, currentBlockNumber, auction.endPeriodBlock);
-				auction.biddingEndsDate = EstimateBlockDate(date, currentBlockNumber, auction.biddingEndsBlock);
-
-				// TODO - how to get his value?
-				auction.startOnBoard = "December 17th, 2021"; 
-				auction.endOnBoard = "October 20th, 2023";
-				//auction.endOnBoard = auction.weeksLeased * 7 + auction.startOnBoard;
-
-				// Calculate next starting block
-				auction.nextStartingBlock = auction.biddingEndsBlock + 3600; // TODO: Get 3600 on-chain??
-				auctionBlocks.push(auction.nextStartingBlock);
-				auctions.push(auction);
-
-				// Drop-down option
-				let option = <option value={i} key={i}>{`Auction #${i + 1} - ${auction.startDate.toDateString()}`}</option>
-				auctionSelections.push(option);
-			}
-
-			Update(auctions, setAuctions, { target: { value: 0 }});
-
+			await Connect(wsProvider, firstAuctionBlockDot, setAuctions);
 		} else if (title === "Parachain Slot Auctions · Guide") {
 			chain = "kusama";
 			wsProvider = new WsProvider("wss://kusama-rpc.polkadot.io/");
-  		api = await ApiPromise.create({ provider: wsProvider });
+			await Connect(wsProvider, firstAuctionBlockKsm, setAuctions);
 		} else {
 			console.log("Unknown wiki/guide type");
 		}
@@ -86,6 +38,57 @@ function AuctionSchedule() {
 	} else {
 		return (<div>Loading Auctions...</div>)
 	}
+}
+
+async function Connect(wsProvider,  initialBlock, setAuctions) {
+	api = await ApiPromise.create({ provider: wsProvider });
+
+	// Get the current block for projection
+	const currentBlock = await api.rpc.chain.getBlock();
+	const currentBlockNumber = parseInt(currentBlock.block.header.number.toString());
+
+	// Get current date/time
+	let date = new Date();
+
+	// Get ending period for the given chain
+	const endPeriod = parseInt(api.consts.auctions.endingPeriod.toString());
+
+	// Add starting block for the given chain
+	let auctions = [];
+	let auctionBlocks = [];
+	auctionBlocks.push(initialBlock);
+
+	// Build auction objects with all required values for UI
+	for (let i = 0; i < auctionCount; i++) {
+		let auction = {};
+		auction.startBlock = auctionBlocks[auctionBlocks.length - 1];
+		auction.startHash = (await api.rpc.chain.getBlockHash(auction.startBlock)).toString();
+		const apiAt = await api.at(auction.startHash);
+		const [lease, end] = (await apiAt.query.auctions.auctionInfo()).toJSON();
+
+		auction.weeksLeased = parseInt(lease);
+		auction.endPeriodBlock = parseInt(end);
+		auction.biddingEndsBlock = auction.endPeriodBlock + endPeriod;
+		auction.startDate = EstimateBlockDate(date, currentBlockNumber, auction.startBlock);
+		auction.endPeriodDate = EstimateBlockDate(date, currentBlockNumber, auction.endPeriodBlock);
+		auction.biddingEndsDate = EstimateBlockDate(date, currentBlockNumber, auction.biddingEndsBlock);
+
+		// TODO - how to get his value?
+		auction.startOnBoard = "December 17th, 2021";
+		auction.endOnBoard = "October 20th, 2023";
+		//auction.endOnBoard = auction.weeksLeased * 7 + auction.startOnBoard;
+
+		// Calculate next starting block
+		auction.nextStartingBlock = auction.biddingEndsBlock + 3600; // TODO: Get 3600 on-chain??
+		auctionBlocks.push(auction.nextStartingBlock);
+		auctions.push(auction);
+
+		// Drop-down option
+		let option = <option value={i} key={i}>{`Auction #${i + 1} - ${auction.startDate.toDateString()}`}</option>
+		auctionSelections.push(option);
+	}
+
+	Update(auctions, setAuctions, { target: { value: 0 } });
 }
 
 function EstimateBlockDate(date, currentBlock, estimatedBlock) {
@@ -107,21 +110,21 @@ function Update(auctions, setAuctions, event) {
 		<hr />
 		<b>Auction Starts:</b>
 		<br />
-		{`${auctions[index].startDate.toDateString()} - `} 
+		{`${auctions[index].startDate.toDateString()} - `}
 		<a href={`https://polkadot.subscan.io/block/${auctions[index].startBlock.toString()}`}>
 			Block #{auctions[index].startBlock.toString()}
 		</a>
 		<hr />
 		<b>Auction Ends:</b>
 		<br />
-		{`${auctions[index].endPeriodDate.toDateString()} - `} 
+		{`${auctions[index].endPeriodDate.toDateString()} - `}
 		<a href={`https://polkadot.subscan.io/block/${auctions[index].endPeriodBlock.toString()}`}>
 			Block #{auctions[index].endPeriodBlock.toString()}
 		</a>
 		<hr />
 		<b>Bidding Ends:</b>
 		<br />
-		{`${auctions[index].biddingEndsDate.toDateString()} - `} 
+		{`${auctions[index].biddingEndsDate.toDateString()} - `}
 		<a href={`https://polkadot.subscan.io/block/${auctions[index].biddingEndsBlock.toString()}`}>
 			Block #{auctions[index].biddingEndsBlock.toString()}
 		</a>
