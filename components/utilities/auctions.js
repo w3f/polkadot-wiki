e7315e3d57b1914aa32c8f6bf9aeeefaa50280f8
alfarok <keith4895@gmail.com>
2022-09-19 21:54:47 -0400

    rough out schema for hard-coded auction data

---
+++ components/utilities/auctions.js
@@ -0,0 +1,14 @@
+const schedule = [
+  {
+    index: 0,
+    option: "1st Auction - Nov 11, 2021",
+    startDate: "November 11th, 2021",
+    startBlock: 7658910,
+    endPeriodDate: "November 13th, 2021",
+    endPeriodBlock: 7684110,
+    endBiddingDate: "November 18th, 2021",
+    endBiddingBlock: 7756110,
+    onboardStart: "December 17th, 2021",
+    onboardEnd: "October 20th, 2023",
+  }
+]
