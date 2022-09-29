b94bc170eb40400d5ee7508263a2f5e3961b8810
je-boska <joneirikboska@gmail.com>
2022-09-29 16:09:19 +0200

    Replace Banner component with built-in announcement bar feature

--- polkadot-wiki/src/pages/index.js
+++ polkadot-wiki/src/pages/index.js
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, {useEffect, useState} from "react";
+import React from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -15,7 +15,6 @@ import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
 import KusamaCanary from "@site/static/img/kusama_canary_white.svg";
-import { useScrollListener } from "../hooks/useScroll";
 
 function HomeNav() {
   const NavContainer = ({ children }) => (
@@ -107,45 +106,6 @@ function HomeNav() {
     </NavContainer>
   );
 }
-
-// Used for advertising events, conferences, etc
-// To do, generalize this, and clean up implementation. This was hacked together for Polkadot Decoded 2022 by Emre
-
-function Banner() {
-  const FixedBanner = () => {
-    const scroll = useScrollListener();
-    const [isScrolled, setIsScrolled] = useState(false);
-  
-    useEffect(() => {
-      if (scroll.y > 200) {
-        setIsScrolled(true);
-      } else if (scroll.y < 1) {
-        setIsScrolled(false);
-      }
-    }, [scroll.y]);
-
-    return (
-      <div className={`fixed-banner ${isScrolled ? 'fixed-banner--hidden' : ''}`}>
-        <div className="mx-auto col-12 col-md-10 col-xl-8 d-flex flex-column flex-md-row align-items-center justify-content-between">
-          <p className="mb-0 text-center text-md-left">
-          <span className="font-weight-bolder pr-lg-4">Sub0 is coming to Lisbon!</span>
-            <span className="d-flex d-lg-inline-block">Join the Polkadot developer conference, November 28th-29th</span>
-          </p>
-          <a href="https://sub0.polkadot.network/?utm_source=wiki.polkadot.network&utm_medium=referral&utm_campaign=sub0%202022&utm_content=notification%20banner%20wiki">
-            <button className="btn btn-md btn-primary btn-bg-primary mt-2 mt-md-0 ml-md-3">
-                Sign Up
-            </button>
-          </a>
-        </div>
-      </div>
-    )
-  };
-
-  return (
-    <FixedBanner />
-  )
-}
-
 
 function HomeFooter() {
   const FooterContainer = ({ children }) => (
@@ -258,7 +218,6 @@ export default function Index() {
 
   return (
     <Layout title={siteConfig.tagline}>
-      <Banner />
       <div className="homeContainer">
         <HomeNav />
         <HomeFooter />
