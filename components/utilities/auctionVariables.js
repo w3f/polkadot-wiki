efbc737f257e87b7e35b971d23b36ebccd470617
alfarok <keith4895@gmail.com>
2022-10-14 04:38:22 -0400

    switch to commonjs for auction variables to support node script

--- components/utilities/auctionVariables.js
+++ components/utilities/auctionVariables.js
@@ -4,24 +4,36 @@ To further reduce latency in the Auction Schedule tool we hard-code them as they
 A regularly scheduled GitHub action will be used to add future auction blocks.
 */
 
-import * as DotAuctions from "./data/PolkadotAuctions.json";
-import * as KsmAuctions from "./data/KusamaAuctions.json";
+let DotAuctions = require("./data/PolkadotAuctions.json");
+let KsmAuctions = require("./data/KusamaAuctions.json");
 
 // consts.slots.leasePeriod
-export const PolkadotSlotLeasePeriod = 1209600;
-export const KusamaSlotLeasePeriod = 604800;
+const PolkadotSlotLeasePeriod = 1209600;
+const KusamaSlotLeasePeriod = 604800;
 
 // consts.slots.leaseOffsets
-export const PolkadotSlotLeaseOffset = 921600;
-export const KusamaSlotLeaseOffset = 0;
+const PolkadotSlotLeaseOffset = 921600;
+const KusamaSlotLeaseOffset = 0;
 
 // consts.auctions.leasePeriodsPerSlot
-export const PolkadotLeasePeriodPerSlot = 8;
-export const KusamaLeasePeriodPerSlot = 8;
+const PolkadotLeasePeriodPerSlot = 8;
+const KusamaLeasePeriodPerSlot = 8;
 
 // GetAuctionBlocks() in Auction-Schedule.jsx can generate all these values from a starting block number
-export const PolkadotAuctions = DotAuctions;
-export const KusamaAuctions = KsmAuctions;
+const PolkadotAuctions = DotAuctions;
+const KusamaAuctions = KsmAuctions;
 
 // A future block that does not yet have an associated hash
-export const FutureBlock = "0x0000000000000000000000000000000000000000000000000000000000000000";
+const FutureBlock = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+module.exports = {
+  PolkadotSlotLeasePeriod,
+  KusamaSlotLeasePeriod,
+  PolkadotSlotLeaseOffset,
+  KusamaSlotLeaseOffset,
+  PolkadotLeasePeriodPerSlot,
+  KusamaLeasePeriodPerSlot,
+  PolkadotAuctions,
+  KusamaAuctions,
+  FutureBlock,
+}
