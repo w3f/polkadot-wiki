06d380b832523c3bfffc53afa0081184a293df75
Keith Alfaro <keith4895@gmail.com>
2022-10-19 11:18:34 +0800

    Auction schedule component should use cache for finalized blocks and use GitHub action for updating cache (#3946)
    * starting roughing out new GH action
    
    * rough out sample json read/write node script
    
    * rough out json update routine
    
    * fix yarn command
    
    * indentation in yml
    
    * test
    
    * leverage script to update json cache
    
    * use cache for finalized dates
    
    * remove un-used loading msg
    
    * consolidation
    
    * more organization
    
    * diff formatting
    
    * comments
    
    * start prepping to run auction:update node script once again for future block updating
    
    * switch to commonjs for auction variables to support node script
    
    * comments
    
    * syntax errors
    
    * replace several now finalized block hashes for polkadot from node script
    
    * update finalized blocks for Kusama using node script
    
    * clean up
    
    * update remaining available block dates for Polkadot
    
    * date check should run after block hash update check
    
    * improve abstraction for updating values for both chains
    
    * let to const
    
    * loop process for both chains, add comments
    
    * add bidding starts calc to GetAuctionBlocks func
    
    * comments on next step to avoid second iteration
    
    * comment
    
    * major clean up and updates
    
    * remove experimental modules flag
    
    * add nulls to kusama cache
    
    * update and fix kusama cache
    
    * restore script for both chains
    
    * update auction schedule component to adopt new cache schema
    
    * exit script when cache updates for both chains completes async
    
    * clean up and comments
    
    * test GitHub action
    
    * github action test
    
    * syntax error
    
    * add git config
    
    * test
    
    * action cleanup
    
    * indentation
    
    * test
    
    * clean up
    
    * add quotes
    
    * revert
    
    * remove debugging GitHub action trigger

--- components/utilities/auctionVariables.js
+++ components/utilities/auctionVariables.js
@@ -1,24 +1,52 @@
 /*
-There are utility functions in Auction-Schedule.jsx to retrieve all these values on-chain.
-To further reduce latency in the Auction Schedule tool we hard-code them as they have yet to change w/ time.
-A regularly scheduled GitHub action will be used to add future auction blocks.
+There are utility functions in updateAuction.js to retrieve all these values on-chain.
+To further reduce latency in the Auction Schedule tool we hard-code them as they have yet 
+to change w/ time or are derived from blocks that have already reach finality.
+A regularly scheduled GitHub action will be used to cache future auction blocks data.
 */
 
-import * as DotAuctions from "./data/PolkadotAuctions.json";
-import * as KsmAuctions from "./data/KusamaAuctions.json";
+const DotAuctions = require("./data/PolkadotAuctions.json");
+const KsmAuctions = require("./data/KusamaAuctions.json");
 
 // consts.slots.leasePeriod
-export const PolkadotSlotLeasePeriod = 1209600;
-export const KusamaSlotLeasePeriod = 604800;
+const PolkadotSlotLeasePeriod = 1209600;
+const KusamaSlotLeasePeriod = 604800;
 
 // consts.slots.leaseOffsets
-export const PolkadotSlotLeaseOffset = 921600;
-export const KusamaSlotLeaseOffset = 0;
+const PolkadotSlotLeaseOffset = 921600;
+const KusamaSlotLeaseOffset = 0;
 
 // consts.auctions.leasePeriodsPerSlot
-export const PolkadotLeasePeriodPerSlot = 8;
-export const KusamaLeasePeriodPerSlot = 8;
+const PolkadotLeasePeriodPerSlot = 8;
+const KusamaLeasePeriodPerSlot = 8;
+
+// Auction starting phases (45 hours)
+const PolkadotStartingPhase = 27000;
+const KusamaStartingPhase = 27000;
+
+// const.auctions.endingPeriod (5 days)
+const PolkadotEndingPeriod = 72000;
+const KusamaEndingPeriod = 72000;
 
 // GetAuctionBlocks() in Auction-Schedule.jsx can generate all these values from a starting block number
-export const PolkadotAuctions = DotAuctions;
-export const KusamaAuctions = KsmAuctions;
+const PolkadotAuctions = DotAuctions;
+const KusamaAuctions = KsmAuctions;
+
+// A future block that does not yet have an associated hash
+const FutureBlock = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+module.exports = {
+  PolkadotSlotLeasePeriod,
+  KusamaSlotLeasePeriod,
+  PolkadotSlotLeaseOffset,
+  KusamaSlotLeaseOffset,
+  PolkadotLeasePeriodPerSlot,
+  KusamaLeasePeriodPerSlot,
+  PolkadotStartingPhase,
+  KusamaStartingPhase,
+  PolkadotEndingPeriod,
+  KusamaEndingPeriod,
+  PolkadotAuctions,
+  KusamaAuctions,
+  FutureBlock,
+}
