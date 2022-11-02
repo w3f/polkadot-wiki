d6ccbd7d811ee5e30ec3d6a5ea8236ce354a4d0f
alfarok <keith4895@gmail.com>
2022-11-02 09:53:54 -0400

    comments

--- scripts/auditLinks.py
+++ scripts/auditLinks.py
@@ -81,18 +81,20 @@ def main():
     emptyLog += "|---|---|---|---|\n"
     # New log
     log = emptyLog
-
+    # Walk the root directory looking for markdown files
     for path, subdirs, files in os.walk(Root):
         for name in files:
             if name[-3:] == ".md":
                 fullPath = os.path.join(path, name)
                 shortPath = fullPath.split("docs")[1]
+                # Attempt to read file, extract links and get the page slug identifier
                 results = parseMarkdown(fullPath)
                 links = results[0]
                 slug = results[1]
                 if links != {}:
                     # For each link
                     for key in links:
+                        # Log to console (not audit report)
                         print("Validating: " + key + " - " + links[key])
 
                         # Url not supported by regex parser
@@ -155,7 +157,8 @@ def main():
                             url = PolkadotUrl + links[key]
                             test = testLink(url)
                             logger(test, log, shortPath, links, key)
-
+    
+    # Log to console (not audit report)
     print()
     print("Audit Complete.")
     print()
@@ -165,6 +168,7 @@ def main():
     if log == emptyLog:
         return False
     else:
+        # If an audit report was generated, append report to a new GitHub issue (markdown)
         with open("Audit-Results.md", "w") as text_file:
             ghIssue = "---\n"
             ghIssue += "title: Monthly Audit of Wiki/Guide Links\n"
