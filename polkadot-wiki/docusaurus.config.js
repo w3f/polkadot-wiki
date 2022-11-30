0fc69a1861f2b26ff96e6c548a0f5069895b38c7
filippoweb3 <filippo@web3.foundation>
2022-11-30 12:58:09 +0100

    Added redirect for learn-account-transfer
    to learn-account-advanced

--- polkadot-wiki/docusaurus.config.js
+++ polkadot-wiki/docusaurus.config.js
@@ -83,6 +83,10 @@ module.exports = {
           {
             to: '/docs/learn-xcm',
             from: ['/docs/learn-crosschain', '/docs/learn-cross-consensus']
+          },
+          {
+            to: '/docs/learn-account-advanced',
+            from: ['/docs/learn-account-transfer']
           }
         ],
         createRedirects: function (existingPath) {
