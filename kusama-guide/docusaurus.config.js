743bca7554a2ec455aa5c87455f4c569e44cdb2c
alfarok <keith4895@gmail.com>
2023-01-19 11:36:25 -0500

    Remove unused dependencies

--- kusama-guide/docusaurus.config.js
+++ kusama-guide/docusaurus.config.js
@@ -1,9 +1,5 @@
-const { baseUrlPattern } = require("../scripts/utils");
 const { injectPlugin } = require("../scripts/injectPlugin");
-
 const i18n = require("./i18n");
-
-const isBuilding = process.env.BUILDING === "true";
 const isPub = process.env.PUBLISHING === "true";
 
 module.exports = {
