84dfa6647f33ebd9fa407d833bd488b9ee44f092
filippoweb3 <filippo@web3.foundation>
2023-04-12 12:00:43 +0200

    added redirect

--- polkadot-wiki/docusaurus.config.js
+++ polkadot-wiki/docusaurus.config.js
@@ -150,6 +150,10 @@ module.exports = {
           {
             to: '/docs/learn-comparisons-kusama',
             from: ['/docs/learn-kusama-vs-polkadot']
+          },
+          {
+            to: '/docs/learn-cryptography',
+            from: ['/docs/learn-randomness']
           }
         ],
         createRedirects: function (existingPath) {
