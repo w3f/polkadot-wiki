4a4a8cc83461aba6e7e541026adf65ee90870813
Filippo <110459737+filippoweb3@users.noreply.github.com>
2023-06-22 15:44:35 +0200

    Wiki Reorg: Unify Ledger pages (#4941)
    * added subid guide to guides
    
    * removed kusama ledger page
    
    - added redirects
    
    * fixed broken links
    
    * changed title
    
    * Update docs/general/ledger.md
    
    Co-authored-by: Radha <86818441+DrW3RK@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Radha <86818441+DrW3RK@users.noreply.github.com>

--- kusama-guide/sidebars.js
+++ kusama-guide/sidebars.js
@@ -97,7 +97,7 @@ module.exports = {
           type: "category",
               label: "Ledger",
               items: [
-                "general/kusama/kusama-ledger",
+                "general/ledger",
                 "general/kusama/kusama-statemine-ledger",
               ],
         },
