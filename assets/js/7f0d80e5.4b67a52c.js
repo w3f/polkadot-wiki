9071c0776a0bfc7b9826296a26a961d7adbacc8e
Polkadot Wiki CI <polkadot-wiki-deploy@users.noreply.github.com>
2023-07-01 00:17:30 +0000

    Deploy website - based on 6b9364c0e60775d94fd1e76c94b91b55d64aaea1

--- assets/js/7f0d80e5.4b67a52c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[5187],{89743:e=>{e.exports=JSON.parse('{"title":"General Information","description":"General Information to get started with Polkadot & Web3.","slug":"/general","permalink":"/docs/general","navigation":{"next":{"title":"Getting Started","permalink":"/docs/getting-started"}}}')}}]);
