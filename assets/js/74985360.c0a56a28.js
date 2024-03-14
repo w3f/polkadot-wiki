a68a3d817998b93b0f9f509433f131b0eecf9e6b
Polkadot Wiki CI <polkadot-wiki-deploy@users.noreply.github.com>
2024-03-14 16:08:53 +0000

    Deploy website - based on 84442dd6d45552874771336980330bf1ad465b95

--- assets/js/74985360.c0a56a28.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[43],{62402:e=>{e.exports=JSON.parse('{"title":"Explore","description":"Explore Wallets, Apps and Programmes in Polkadot and Kusama ecosystems.","slug":"/explore-index","permalink":"/docs/explore-index","navigation":{"next":{"title":"Getting Started","permalink":"/docs/getting-started"}}}')}}]);
