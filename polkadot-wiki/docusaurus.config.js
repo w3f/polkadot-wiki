3ea6d6ed8e25875899cf2845b02ddf0386a69767
Filippo <filippo@web3.foundation>
2024-07-18 10:17:45 +0200

    deactivate bar

--- polkadot-wiki/docusaurus.config.js
+++ polkadot-wiki/docusaurus.config.js
@@ -281,14 +281,14 @@ module.exports = {
     },
     docsSideNavCollapsible: true,
     /* Banner / Announcement bar */
-    announcementBar: {
-      id: "banner",
-      content:
-        'The Polkadot Blockchain Academy is coming to Asia! <a href="https://www.polkadot.network/development/academy/?utm_source=wiki.polkadot.network&utm_medium=referral&utm_campaign=pba%204%205&utm_content=notification" target="_blank" rel="noopener noreferrer">Find out more 👉</a>',
-      backgroundColor: '#e6007a',
-      textColor: 'white',
-      isCloseable: true,
-    },
+//    announcementBar: {
+//      id: "banner",
+//      content:
+//        'The Polkadot Blockchain Academy is coming to Asia! <a href="https://www.polkadot.network/development/academy/?utm_source=wiki.polkadot.network&utm_medium=referral&utm_campaign=pba%204%205&utm_content=notification" target="_blank" rel="noopener noreferrer">Find out more 👉</a>',
+//      backgroundColor: '#e6007a',
+//      textColor: 'white',
+//      isCloseable: true,
+//    },
     image: 'img/og-polkadot-wiki.png'
   },
 };
