e6c8de516555b3024bf910fb984c33b49ef85650
Bader Youssef <ibnbassem@gmail.com>
2025-03-25 17:22:39 -0400

    resize image + message box fix

--- docs/javascript/MessageBox.js
+++ docs/javascript/MessageBox.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const messageBox = document.getElementById("messageBox");
   const closeButton = document.querySelector(".close-messagebox");
-
-  // Add event listener to close button
-  closeButton.addEventListener("click", () => {
-    messageBox.classList.add("hidden");
-  });
+  if (closeButton !== null) {
+    // Add event listener to close button
+    closeButton.addEventListener("click", () => {
+      messageBox.classList.add("hidden");
+    });
+  }
 });
-
