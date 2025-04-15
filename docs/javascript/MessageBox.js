afb37afa9fee65d15c168fc2f1270f8d06a18525
Bader Youssef <ibnbassem@gmail.com>
2025-04-15 15:18:59 -0400

    Fix message box event

--- docs/javascript/MessageBox.js
+++ docs/javascript/MessageBox.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("click", () => {
   const messageBox = document.getElementById("messageBox");
   const closeButton = document.querySelector(".close-messagebox");
   if (closeButton !== null) {
-    // Add event listener to close button
     closeButton.addEventListener("click", () => {
       messageBox.classList.add("hidden");
     });
