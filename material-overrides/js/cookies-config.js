7f359c3c06872971bf94328e695b37e39fca1682
Filippo <filippo@web3.foundation>
2025-07-29 14:29:38 +0200

    edits

--- material-overrides/js/cookies-config.js
+++ material-overrides/js/cookies-config.js
@@ -10,7 +10,7 @@ window.addEventListener("load", function () {
       dismiss: "Accept",
       deny: "Decline",
       link: "See the Privacy Policy.",
-      href: "https://docs.polkadot.com/privacy-policy/"
+      href: "/policies/legal-disclosures",
     },
     onInitialise: function (status) {
       if (status === 'allow') {
@@ -60,7 +60,7 @@ window.addEventListener("load", function () {
     script.setAttribute('data-project-logo', 'https://1000logos.net/wp-content/uploads/2022/08/Polkadot-Symbol.png');
     script.setAttribute('data-modal-header-bg-color', '#1C0533');
     script.setAttribute('data-modal-title-color', 'white');
-    script.setAttribute('data-modal-disclaimer', `This is an AI chatbot trained to answer questions about Polkadot. As such, the answers it provides might not always be accurate or up-to-date. Please use your best judgement when evaluating its responses. Also, **please refrain from sharing any personal or private information with the bot**. By submitting a query, you agree that you have read and understood [these conditions](https://polkadot.com/legal-disclosures/).
+    script.setAttribute('data-modal-disclaimer', `This is an AI chatbot trained to answer questions about Polkadot. As such, the answers it provides might not always be accurate or up-to-date. Please use your best judgement when evaluating its responses. Also, **please refrain from sharing any personal or private information with the bot**. By submitting a query, you agree that you have read and understood [these conditions](/policies/legal-disclosures).
         **If you need further assistance, you can reach out to [Polkadot Support](https://support.polkadot.network/support/tickets/new?ticket_form=i_have_a_support_request).**`);
     script.setAttribute('data-modal-example-questions', 'Where can I store my DOT?, How can I create a DOT account?, How can I stake my DOT?, How does Polkadot OpenGov work?');
     script.setAttribute('data-modal-disclaimer-font-size', '12px');
