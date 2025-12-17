9a5ba942506f76e68b2d9a457e57a1a53dc18eb9
Bader Youssef <ibnbassem@gmail.com>
2025-12-17 11:15:09 -0500

    Update AI popup and favicon

--- material-overrides/js/cookies-config.js
+++ material-overrides/js/cookies-config.js
@@ -52,8 +52,8 @@ window.addEventListener("load", function () {
     script.setAttribute('data-project-name', 'Polkadot');
     script.setAttribute('data-modal-title', 'Polkadot AI Chatbot');
     script.setAttribute('data-project-color', '#1E1E1E');
-    script.setAttribute('data-button-bg-color', '#1C0533');
-    script.setAttribute('data-button-text-color', 'white');
+    script.setAttribute('data-button-bg-color', '#1E1E1E');
+    script.setAttribute('data-button-text-color', '#F5F5F5');
     script.setAttribute('data-button-height', '80px');
     script.setAttribute('data-button-width', '72px');
     script.setAttribute('data-font-size-xs', '12px');
@@ -68,9 +68,9 @@ window.addEventListener("load", function () {
     script.setAttribute('data-button-image-width', '24px');
     script.setAttribute('data-modal-image-height', '24px');
     script.setAttribute('data-modal-image-width', '24px');
-    script.setAttribute('data-project-logo', 'https://1000logos.net/wp-content/uploads/2022/08/Polkadot-Symbol.png');
-    script.setAttribute('data-modal-header-bg-color', '#1C0533');
-    script.setAttribute('data-modal-title-color', 'white');
+    script.setAttribute('data-project-logo', 'https://docs.polkadot.com/assets/images/polkadot.png');
+    script.setAttribute('data-modal-header-bg-color', '#1E1E1E');
+    script.setAttribute('data-modal-title-color', '#F5F5F5');
     script.setAttribute('data-modal-disclaimer', `This is an AI chatbot trained to answer questions about Polkadot. As such, the answers it provides might not always be accurate or up-to-date. Please use your best judgement when evaluating its responses. Also, **please refrain from sharing any personal or private information with the bot**. By submitting a query, you agree that you have read and understood [these conditions](/policies/chatbot-terms).
         **If you need further assistance, you can reach out to [Polkadot Support](https://support.polkadot.network/support/tickets/new?ticket_form=i_have_a_support_request).**`);
     script.setAttribute('data-modal-example-questions', 'Where can I store my DOT?, How can I create a DOT account?, How can I stake my DOT?, How does Polkadot OpenGov work?');
